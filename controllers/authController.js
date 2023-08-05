a3cc418a64eea5952be6cd260d28605c6d559389
Shivam Puri <puri46394@gmail.com>
2023-08-05 15:22:53 +0530

    users are now registering

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,57 @@
 // This file contains the functions/controllers  related to authentication and are called when user is on any authentication related route like /register or /login
+import { hashPassword } from '../helpers/authHelper.js';
+import userModel from '../models/userModel.js'
 
-export const registerController = () => {}
+export const registerController = async (req, res) => {
+    try {
+        const { name, email, password, phone, address } = await req.body
+
+        // validation messages 
+        if (!name) {
+            return res.send({ error: 'Name is required' });
+        }
+        if (!email) {
+            return res.send({ error: 'Email is required' });
+        }
+        if (!password) {
+            return res.send({ error: 'Password is required' });
+        }
+        if (!phone) {
+            return res.send({ error: 'Phone is required' });
+        }
+        if (!address) {
+            return res.send({ error: 'Address is required' });
+        }
+
+        // check if user already exists ?
+        const existingUser = await userModel.findOne({ email })
+
+        // if user already exists
+        if (existingUser) {
+            return res.status(200).send({
+                success: true,
+                message: 'User already registered, please login'
+            })
+        }
+
+        // register user 
+        const hashedPassword = await hashPassword(password);
+        // save user in the database 
+        const user = new userModel({
+            name, email, password: hashedPassword, phone, address
+        }).save()
+
+        res.status(201).send({
+            success: true,
+            message: 'User registered successfully'
+        })
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({
+            success: false,
+            message: 'Error in registeration',
+            error
+        })
+    }
+}
--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 <!-- npm init -y -->
 <!-- Installed express using --> npm i express
-<!-- created server.js upto block - a -->
+<!-- created server.js -->
 <!-- installed supervisor dotenv and morgan--> npm i supervisor dotenv morgan
 <!-- morgon : for logging HTTP requests and responses in Node. js applications. -->
 <!-- created a shared cluster named ecommerce on mongoDB -->
@@ -8,3 +8,4 @@
 
 <!-- installed mongoose : enables developers to define data models with fields, data types, validation rules, and default values, making it easier to ensure data consistency and integrity.
  --> npm i mongoose
+ <!-- bcrypt for encrypting passwords -->
